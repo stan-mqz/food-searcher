7154f2af6e81ed87ad17e6c56112fd38711739b5
Stan <moscarstanley37@gmail.com>
2024-12-24 11:02:50 -0600

    feat: crated api response type validation with zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import { FoodForm } from "./components/FoodForm";
 import { SelectFood } from "./components/SelectFood";
+import useFood from "./hooks/useFood";
 
 function App() {
-  
+
+  const {fetchFood} = useFood();
+
+  useEffect(() => {
+    fetchFood()
+  }, [])
+
   return (
     <>
       <h1 className="text-center text-4xl font-bold mt-6">
--- src/hooks/useFood.ts
+++ src/hooks/useFood.ts
@@ -1,13 +1,41 @@
+import { z } from "zod";
+
+const foodCathegories = z.object({
+  categories: z.array(
+    z.object({
+      idCategory: z.string(),
+      strCategory: z.string(),
+      strCategoryThumb: z.string(),
+      strCategoryDescription: z.string(),
+    })
+  ),
+});
+
+export type foodCathegories = z.infer<typeof foodCathegories>;
+
 export default function useFood() {
-    
+  const URL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
-    const URL = 'www.themealdb.com/api/json/v1/1/categories.php'
+  const fetchFood = async () => {
+    try {
+      const response = await fetch(URL);
 
-    
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
 
+      const data = await response.json();
 
+      const result = foodCathegories.safeParse(data);
 
-    return {
-
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  };
+
+  return {
+    fetchFood,
+  };
 }
