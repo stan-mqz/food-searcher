5524a19c6c4805b9318bff06e170d8456e3dd063
Stan <moscarstanley37@gmail.com>
2024-12-24 11:56:36 -0600

    style : Created options for select in SelectFood based on API data

--- src/components/SelectFood.tsx
+++ src/components/SelectFood.tsx
@@ -1,8 +1,21 @@
+import { useFoodStore } from "../store/store";
 
 export const SelectFood = () => {
+  const foodCathegory = useFoodStore((state) => state.foodCathegory);
+
   return (
-    <select className="border border-black rounded-md w-28 p-2" name="" id="">
-        <option className="" value="">Hola</option>
+    <select
+    name=""
+      id="cathegory"
+      className="border border-black rounded-md w-28 p-2"
+    >
+      {foodCathegory.map((cathegory) => {
+        return cathegory.categories.map((category) => (
+          <option key={category.idCategory} value={category.idCategory}>
+            {category.strCategory}
+          </option>
+        ));
+      })}
     </select>
-  )
-}
+  );
+};
