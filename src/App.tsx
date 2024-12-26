b59d5c70802649ca58e75a95bf4e90614f1bd70d
Stan <moscarstanley37@gmail.com>
2024-12-26 16:30:32 -0600

    style: reposition Clear Search button and enhance layout for meal and category display

--- src/App.tsx
+++ src/App.tsx
@@ -19,43 +19,49 @@ function App() {
     <>
       <NavBar />
 
-      <div className="flex justify-end w-full">
-        {" "}
-        <button
-          className="bg-blue-950 text-white font-bold rounded-md m-2 border-white border p-2 w-28 disabled:opacity-25"
-          disabled={singleFoodCathegory?.meals?.length === 0}
-          onClick={clearState}
-        >
-          Clear Search
-        </button>
-      </div>
-
       {singleFoodCathegory?.meals?.length > 0 ? (
-        <div className="grid grid-cols-3 gap-2 mx-auto mt-10 p-2">
-          {singleFoodCathegory?.meals?.map((meal) => (
-            <SingleCathegoryCard
-              key={meal.idMeal}
-              mealName={meal.strMeal}
-              mealImage={meal.strMealThumb}
-              mealInstructions={meal.strInstructions}
-              mealIngredients={getIngredients(meal)}
-              mealCategory={meal.strCategory}
-              mealArea={meal.strArea}
-              mealYtVideo={meal.strYoutube}
-            />
-          ))}
-        </div>
+        <>
+          <div className="flex justify-end w-full">
+            {" "}
+            <button
+              className="bg-blue-950 text-white font-bold rounded-md mt-10 mr-5 border-white border p-2 w-28"
+              onClick={clearState}
+            >
+              Clear Search
+            </button>
+          </div>
+          <div className="grid grid-cols-3 gap-2 mx-auto mt-10 p-2">
+            {singleFoodCathegory?.meals?.map((meal) => (
+              <SingleCathegoryCard
+                key={meal.idMeal}
+                mealName={meal.strMeal}
+                mealImage={meal.strMealThumb}
+                mealInstructions={meal.strInstructions}
+                mealIngredients={getIngredients(meal)}
+                mealCategory={meal.strCategory}
+                mealArea={meal.strArea}
+                mealYtVideo={meal.strYoutube}
+              />
+            ))}
+          </div>
+        </>
       ) : (
-        <div className="grid grid-cols-4 max-w-[90%] place-content-between gap-2 mx-auto mt-10 p-2">
-          {foodCathegory?.categories?.map((category) => (
-            <FoodCard
-              key={category.idCategory}
-              cathegoryName={category.strCategory}
-              foodDescription={category.strCategoryDescription}
-              bgImage={category.strCategoryThumb}
-            />
-          ))}
-        </div>
+        <>
+          <h2 className="text-3xl font-bold text-center mt-5">
+            Food Cathegories
+          </h2>
+
+          <div className="grid grid-cols-4 max-w-[90%] place-content-between gap-2 mx-auto mt-10 p-2">
+            {foodCathegory?.categories?.map((category) => (
+              <FoodCard
+                key={category.idCategory}
+                cathegoryName={category.strCategory}
+                foodDescription={category.strCategoryDescription}
+                bgImage={category.strCategoryThumb}
+              />
+            ))}
+          </div>
+        </>
       )}
 
       <FoodModal />
